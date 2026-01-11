ab3e0fdad5892d34bc44672e96f23f2c12beacac
shreevidyamatabhuvanesvari-gif <shreevidyamatabhuvanesvari@gmail.com>
2026-01-11 20:20:01 +0530

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,14 +1,14 @@
 const text = document.getElementById("anjaliText");
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.lang="hi-IN";
+
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+recognition.lang = "hi-IN";
 
 function startListening(){
   recognition.start();
-  text.innerText="सुन रही हूँ… 🎧";
+  text.innerText = "सुन रही हूँ… 💕";
 }
 
-recognition.onresult = (e)=>{
+recognition.onresult = (e) => {
   const user = e.results[0][0].transcript;
   const reply = getAnswer(user);
   text.innerText = reply;
@@ -16,7 +16,7 @@ recognition.onresult = (e)=>{
 };
 
 function speak(msg){
-  const u = new SpeechSynthesisUtterance(msg);
-  u.lang="hi-IN";
-  speechSynthesis.speak(u);
+  const speech = new SpeechSynthesisUtterance(msg);
+  speech.lang = "hi-IN";
+  speechSynthesis.speak(speech);
 }
